d13cef86244ae5fe4988e9755582f15200eaa1e0
Emre Sokullu <esokullu@gmail.com>
2020-04-26 18:30:22 -0700

    first commit

---
+++ bin/groupsville.php
@@ -0,0 +1,15 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+use Groupsville\Cli;
+
+$version = file_get_contents(dirname(__DIR__) . DIRECTORY_SEPARATOR . "VERSION");
+$application = new Application('Groupsville', $version);
+$application->add(new Cli\BuildCommand());
+$application->add(new Cli\InitCommand());
+//$application->add(new Cli\ServeCommand());
+//$application->add(new Cli\RunCommand());
+$application->run();
